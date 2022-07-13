b6b82f0b583803e180274dd9160fcd67ad1451d0
jgarcia5n <83414091+jgarcia5n@users.noreply.github.com>
2022-07-13 14:25:52 -0500

    Uploaded by Quasar Publish Script

---
+++ assets/pinia.5abfb583.js
@@ -0,0 +1,1 @@
+import{f as e,j as i}from"./index.42800975.js";var r=e(({app:a})=>{a.use(i())});export{r as default};
--- assets/use-quasar.4458ab42.js
+++
@@ -1,1 +0,0 @@
-import{i as a,cw as r}from"./index.651b35db.js";function u(){return a(r)}export{u};
