147dffe5d89451cdebee743792156f086c1ae225
Robert Bel <rbel@brandsafway.com>
2023-01-13 16:53:38 -0600

    Uploaded by Quasar Publish Script

---
+++ assets/FullLayoutStart.8493a4e2.js
@@ -0,0 +1,1 @@
+import{a as o,Q as r}from"./QLayout.81df143e.js";import{_ as n,aZ as s,A as c,an as _,bi as e,d as t}from"./index.0dddbf49.js";import"./QResizeObserver.79867edd.js";const f={};function i(l,u){const a=s("router-view");return c(),_(r,{view:"hHh lpR fFf"},{default:e(()=>[t(o,null,{default:e(()=>[t(a)]),_:1})]),_:1})}var v=n(f,[["render",i]]);export{v as default};
--- assets/use-quasar.ed3977b0.js
+++
@@ -1,1 +0,0 @@
-import{i as a,cv as r}from"./index.b48bafa3.js";function u(){return a(r)}export{u};
