b52aa392d00c867ac60b220cdd38050e8f9626be
jgarcia5n <83414091+jgarcia5n@users.noreply.github.com>
2023-08-03 00:30:26 -0500

    Uploaded by Quasar Publish Script

---
+++ assets/socketListener.1f5c3081.js
@@ -0,0 +1,1 @@
+import{api as i}from"./api.0b402b2c.js";const e=i.feathersClient,s=e?e.service("notifications"):null,n=e?e.service("messages"):null,c={notificationService:s,messagingService:n};export{c as l};
---
+++ assets/use-quasar.4af20f9f.js
@@ -0,0 +1,1 @@
+import{i as a,cR as r}from"./index.dbae30bb.js";function u(){return a(r)}export{u};
