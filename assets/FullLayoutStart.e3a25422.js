70d90031a81fd94b01c0d790911d3f37d5cb86e2
jgarcia5n <83414091+jgarcia5n@users.noreply.github.com>
2023-08-07 08:38:58 -0500

    Uploaded by Quasar Publish Script

--- assets/FullLayoutStart.e3a25422.js
+++
@@ -1,1 +0,0 @@
-import{Q as o,a as r}from"./QLayout.a16aaad5.js";import{_ as n,b5 as s,A as c,aq as _,br as e,d as t}from"./index.43f38233.js";import"./QResizeObserver.d3fd7deb.js";const f={};function l(u,i){const a=s("router-view");return c(),_(r,{view:"hHh lpR fFf"},{default:e(()=>[t(o,null,{default:e(()=>[t(a)]),_:1})]),_:1})}var v=n(f,[["render",l]]);export{v as default};
