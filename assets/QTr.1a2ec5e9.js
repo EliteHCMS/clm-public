a30bd74c44afd2baa8b949249b06ef90ea9ce23b
jgarcia5n <83414091+jgarcia5n@users.noreply.github.com>
2024-04-10 09:00:57 -0500

    Uploaded by Quasar Publish Script

--- assets/QTr.1a2ec5e9.js
+++
@@ -1,1 +0,0 @@
-import{bZ as t,c as s,h as a,b_ as n}from"./index.8838c464.js";var c=t({name:"QTr",props:{props:Object,noHover:Boolean},setup(r,{slots:e}){const o=s(()=>"q-tr"+(r.props===void 0||r.props.header===!0?"":" "+r.props.__trClass)+(r.noHover===!0?" q-tr--no-hover":""));return()=>a("tr",{class:o.value},n(e.default))}});export{c as Q};
--- assets/use-quasar.ed4be5d9.js
+++
@@ -1,1 +0,0 @@
-import{i as a,cU as r}from"./index.8838c464.js";function u(){return a(r)}export{u};
